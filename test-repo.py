759b352effe534b8e51117eb8772183bb65689ed
Marius van den Beek <m.vandenbeek@gmail.com>
2015-06-30 20:18:38 +0200

    Implement --skip_test to skip tests when doing shed_test and there are no changes as compared to the verion installed on the target shed.

--- test-repo.py
+++ test-repo.py
@@ -68,7 +68,7 @@ def prepare_html(current_report_dir, build_number, tests):
     with open("{0}/index.html".format(current_report_dir),'w') as index:
         [index.write(line+'\n') for line in result]
 
-def construct_cmds(tests, action, shed_target=None, api_keys=None):
+def construct_cmds(tests, action, shed_target=None, api_keys=None, skip_test=False):
     '''
     prepare test commands
     '''
@@ -93,16 +93,17 @@ def construct_cmds(tests, action, shed_target=None, api_keys=None):
             update_cmd = ""
             for toolshed in test['toolshed']:
                 if toolshed==shed_target:
-                    if skip_test(test['test_directory'], toolshed):
-                        cmds.append ("echo \"{0} tool unchanged, skip testing\"".format(test['name']))
-                        continue
+                    if skip_test:
+                        if test_skippable(test['test_directory'], toolshed):
+                            cmds.append ("echo \"{0} tool unchanged, skip testing\"".format(test['name']))
+                            continue
                     update_cmd = "cd {0} && planemo shed_update --force_repository_creation --shed_target {1} --shed_key {2} . || true && ".format(test['test_directory'], toolshed, api_key )
                     cmd = "cd {0} && planemo shed_test --install_galaxy --test_output_xunit {1}  --test_output {2} --shed_target {3} --shed_key {4} .".format(test['test_directory'], test['test_output_xunit'], test['test_output'], shed_target, api_key )
                     cmd = update_cmd+cmd
                     cmds.append(cmd)
     return cmds
 
-def skip_test(test_dir, shed_target):
+def test_skippable(test_dir, shed_target):
     cmd = "cd {0} && planemo shed_diff --shed_target {1}".format(test_dir, shed_target)
     rc = subprocess.call(cmd, shell=True)
     if rc == 0:
@@ -135,22 +136,13 @@ def run_test(args, current_report_dir):
     cmds = construct_cmds ( tests, args.command, None, api_keys)
     return cmds
 
-def run_shed_test(args, current_report_dir, api_keys):
+def run_shed_test(args, current_report_dir, api_keys, skip_test):
     '''
     Run tests by using planemo shed_test, which installs tools and tool dependencies from the toolshed
     '''
     tests = prepare_tests(args.tool_dirs, current_report_dir, args.report_dir, api_keys)
     prepare_html(current_report_dir, args.build_number, tests)
-    cmds = construct_cmds ( tests, args.command, args.shed_target, api_keys)
-    return cmds
-
-def run_shed_test_incremental(args, current_report_dir, api_keys):
-    '''
-    Run tests by using planemo shed_test, which installs tools and tool dependencies from the toolshed
-    '''
-    tests = prepare_tests(args.tool_dirs, current_report_dir, args.report_dir, api_keys)
-    prepare_html(current_report_dir, args.build_number, tests)
-    cmds = construct_cmds ( tests, args.command, args.shed_target, api_keys)
+    cmds = construct_cmds ( tests, args.command, args.shed_target, api_keys, skip_test)
     return cmds
 
 if __name__ == "__main__":
@@ -167,6 +159,8 @@ if __name__ == "__main__":
                                              description="Upload tool to indicated toolshed and run functional tests within a disposable galaxy instance. Tool and dependencies will be downloaded from the indicated toolshed.", parents=[parent])
     parser_update = arguments.shed_args(parser_update)
     parser_shed_test = arguments.shed_args(parser_shed_test)
+    parser_shed_test.add_argument('--skip_test', dest="skip_test", action="store_true", \
+                                  help='If there is no difference between toolshed and local repo, skip testing')
     args = parser.parse_args()
 
     args.report_dir = os.path.abspath(args.report_dir)
@@ -180,7 +174,7 @@ if __name__ == "__main__":
     if args.command == "update":
         cmds = run_update(args, api_keys)
     if args.command == "shed_test":
-        cmds = run_shed_test(args, current_report_dir, api_keys)
+        cmds = run_shed_test(args, current_report_dir, api_keys, args.skip_test)
 
     if args.cores > 1:
         p = Pool(processes=args.cores)
